b9343a72917a448bb9ee1ff6041d6088f1973789
Adria Payet <adriafullstack@gmail.com>
2025-06-21 21:00:43 +0200

    test update for pass

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -3,4 +3,4 @@ def test_root(client):
     """Dummy test"""
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}  # adapt to your response
+    assert response.json() == {"message": "Hello, FastAPI!"}
